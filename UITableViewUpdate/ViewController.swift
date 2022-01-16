dac6200864e3bd2f1c54a3311520bf609296cb4f
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-01-16 13:40:14 +0900

    データの追加と削除を実装できた

--- UITableViewUpdate/ViewController.swift
+++ UITableViewUpdate/ViewController.swift
@@ -6,11 +6,22 @@
 //
 
 import UIKit
+import RealmSwift
+
+class Item: Object {
+    @objc dynamic var title: String = ""
+}
+
+class ItemList: Object {
+    let list = List<Item>()
+}
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    let realm = try! Realm()
+    var list: List<Item>!
+    
     @IBOutlet weak var tableView: UITableView!
-    var languages: [String] = ["Swift", "Java", "Ruby", "Javascript", "PHP"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,15 +30,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         let addBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonPressed(_ :)))
         navigationItem.rightBarButtonItems = [editButtonItem, addBarButtonItem]
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+        list = realm.objects(ItemList.self).first?.list
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return languages.count
+        return realm.objects(Item.self).count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-        cell.textLabel?.text = languages[indexPath.row]
+        cell.textLabel?.text = list[indexPath.row].title
         return cell
     }
     
@@ -35,15 +49,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         var textField = UITextField()
         let alert = UIAlertController(title: "新しいアイテムを追加", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "追加", style: .default) { (action) in
-            self.languages.append(textField.text!)
+            let item = Item()
+            item.title = textField.text!
+            try! self.realm.write() {
+                if self.list == nil {
+                    let itemList = ItemList()
+                    itemList.list.append(item)
+                    self.realm.add(itemList)
+                    self.list = self.realm.objects(ItemList.self).first?.list
+                } else {
+                    self.list.append(item)
+                }
+            }
             self.tableView.reloadData()
-            print(self.languages)
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "アイテムを入力"
             textField = alertTextField
         }
-            
+        
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
@@ -56,18 +80,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            languages.remove(at: indexPath.row)
+            try! realm.write {
+                let item = list[indexPath.row]
+                realm.delete(item)
+            }
             tableView.deleteRows(at: [indexPath], with: .fade)
-            print(languages)
         }
     }
     
     //並び替え時の処理
     func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-        let language = languages[fromIndexPath.row]
-        languages.remove(at: fromIndexPath.row)
-        languages.insert(language, at: to.row)
-        print(languages)
+        try! realm.write {
+            let listItem = list[fromIndexPath.row]
+            list.remove(at: fromIndexPath.row)
+            list.insert(listItem, at: to.row)
+        }
     }
 
     //並び替えを可能にする
